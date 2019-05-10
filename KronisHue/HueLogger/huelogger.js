ef2835f5e14d6acc6e0ca7cb874aad70649c5164
Anders Kronlund <anders.kronlund@gmail.com>
2019-05-10 21:59:56 +0200

    huelogger added

---
+++ KronisHue/HueLogger/huelogger.js
@@ -0,0 +1,103 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const http = require("http");
+const fetch = require("node-fetch");
+var settings = {
+    username: undefined,
+    huebridgeip: "philips-hue",
+    serverinterface: "127.0.0.1",
+    serverport: 3000
+};
+const readFile = (path, opts = 'utf8') => new Promise((resolve, reject) => {
+    fs.readFile(path, opts, (err, data) => {
+        if (err)
+            reject(err);
+        else
+            resolve(data);
+    });
+});
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+server.listen(settings.serverport, settings.serverinterface, () => {
+    console.log(`Server running at http://${settings.serverinterface}:${settings.serverport}/`);
+});
+function registerOnHue() {
+    fetch(`http://${settings.huebridgeip}/api`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ "devicetype": "kronishuelogger" })
+    })
+        .then(res => res.json())
+        .then(response => {
+        if (Array.isArray(response) && response[0] && response[0].success && response[0].success.username) {
+            settings.username = response[0].success.username;
+            saveSettings();
+        }
+        else if (Array.isArray(response) && response[0] && response[0].error && response[0].error.description) {
+            console.log(response[0].error.description);
+        }
+        else {
+            console.log("Unknown response");
+        }
+    });
+}
+function saveSettings() {
+    let data = JSON.stringify(settings, null, 2);
+    fs.writeFile("settings.json", data, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    });
+}
+function loadSettings() {
+    return readFile("settings.json", 'utf8').then((data) => {
+        settings = JSON.parse(data);
+    }).catch((err) => {
+        console.log("An error occured while reading from File.");
+        console.log(err);
+    });
+}
+function checkHueForChange() {
+    if (settings.username !== undefined) {
+    }
+}
+function mainfunc() {
+    if (settings.username === undefined) {
+        console.log("Register app on hue bridge");
+        registerOnHue();
+        if (settings.username === undefined) {
+            setTimeout(mainfunc, 5000);
+            return;
+        }
+    }
+    if (settings.username !== undefined) {
+        console.log("Fetching changes from hue bridge");
+        checkHueForChange();
+        setTimeout(mainfunc, 60000);
+        return;
+    }
+}
+function initapp() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield loadSettings();
+        mainfunc();
+    });
+}
+initapp();
+//# sourceMappingURL=huelogger.js.map
---
+++ KronisHue/HueLogger/huelogger.ts
@@ -0,0 +1,111 @@
+import * as fs from 'fs';
+import * as http from 'http';
+import * as fetch from 'node-fetch';
+import { isMainThread } from 'worker_threads';
+
+var settings = {
+    username:undefined,
+    huebridgeip:"philips-hue",
+    
+    serverinterface:"127.0.0.1",
+    serverport:3000
+}
+
+const readFile = (path:string, opts = 'utf8') =>
+  new Promise<string>((resolve, reject) => {
+    fs.readFile(path, opts, (err, data) => {
+      if (err) 
+        reject(err)
+      else 
+        resolve(data)
+    })
+  })
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(settings.serverport, settings.serverinterface, () => {
+    console.log(`Server running at http://${settings.serverinterface}:${settings.serverport}/`);
+});
+
+function registerOnHue() {
+    fetch(`http://${settings.huebridgeip}/api`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify({"devicetype":"kronishuelogger"})
+    })
+    .then(res => res.json())
+    .then(response => {
+        if(Array.isArray(response) && response[0] && response[0].success && response[0].success.username ){
+            settings.username = response[0].success.username;
+            saveSettings();
+        } 
+        else if(Array.isArray(response) && response[0] && response[0].error && response[0].error.description ){
+            console.log(response[0].error.description);
+        }
+        else
+        {
+            console.log("Unknown response");
+        }
+
+    });
+}
+
+function saveSettings(){
+    let data = JSON.stringify(settings,null, 2)
+
+    fs.writeFile("settings.json", data, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+     
+        console.log("JSON file has been saved.");
+    });
+}
+
+function loadSettings() {
+    return readFile("settings.json", 'utf8' ).then((data)=>{
+        settings = JSON.parse(data);
+    }).catch((err) => {
+        console.log("An error occured while reading from File.");
+        console.log(err);
+    });
+}
+
+function checkHueForChange() {
+}
+
+function mainfunc() {
+    if(settings.username === undefined) {
+        console.log("Register app on hue bridge");
+
+        registerOnHue();
+
+        if(settings.username === undefined) {
+            setTimeout(mainfunc,5000);
+            return;
+        }
+    }
+    if(settings.username !== undefined) {
+        console.log("Fetching changes from hue bridge");
+        
+        checkHueForChange();
+
+        setTimeout(mainfunc,60000);
+        return;
+    }
+}
+
+async function initapp() {
+    await loadSettings();
+    mainfunc();
+}
+
+initapp();
